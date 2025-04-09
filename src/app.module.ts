f84bff3cef3345238a7486e4193f3f468c08dd10
luiggy <luiggylml@gmail.com>
2025-04-09 16:22:08 -0500

    feat: implement Prisma module and service with tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { databaseConfig } from './config/database.config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { databaseConfig } from './config/database.config';
       load: [databaseConfig],
       isGlobal: true,
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
